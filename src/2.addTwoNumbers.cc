9ba3c27426e7ab02785eb213302684b611637b82
Wang-Yann <wzy-511@163.com>
2020-04-27 22:57:26 +0800

    start C

--- src/2.addTwoNumbers.cc
+++ src/2.addTwoNumbers.cc
@@ -13,12 +13,13 @@ TEST(addTwoNumbers, addTwoNumbers_1)
 {
     Solution s;
     vector<int> in = {1, 2, 3};
-    int ans = 1;
+    int ans = 12;
     EXPECT_EQ(s.addTwoNumbers(in), ans);
 }
 
 int main(int argc, char **argv)
 {
+    printf("%s","hello world\n");
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
