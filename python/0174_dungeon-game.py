a368f394b49f89d48544f18de4a3ebe474208c88
Wang-Yann <wzy-511@163.com>
2020-04-29 21:36:28 +0800

    DP

--- python/0174_dungeon-game.py
+++ python/0174_dungeon-game.py
@@ -37,9 +37,9 @@ class Solution:
             dp[m-1][j]=max(1,dp[m-1][j+1]-dungeon[m-1][j])
         for i in range(m-2,-1,-1):
             for j in range(n-2,-1,-1):
-                dpmin = min(dp[i+i][j],dp[i][j+1])
+                dpmin = min(dp[i+1][j],dp[i][j+1])
                 dp[i][j] = max(1,dpmin-dungeon[i][j])
-        print(dp)
+        # print(dp)
         return dp[0][0]
 
 
