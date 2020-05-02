f807c013a30dfb19c02a373d251809bfd2ffc260
Wang-Yann <wzy-511@163.com>
2020-05-02 23:37:53 +0800

    C string

--- test/test_0020.cpp
+++ test/test_0020.cpp
@@ -16,10 +16,12 @@ TEST(isValid, test1) {
     char s2[]= "(]";
     char s3[] = "()";
     char s4[] = "([)]";
+    char s5[] = "(]";
     ASSERT_EQ(isValid(s1), true);
-    ASSERT_EQ(isValid(s2), true);
+    ASSERT_EQ(isValid(s2), false);
     ASSERT_EQ(isValid(s3), true);
-    ASSERT_EQ(isValid(s4), true);
+    ASSERT_EQ(isValid(s4), false);
+    ASSERT_EQ(isValid(s5), false);
 }
 
 int main(int argc, char **argv) {
