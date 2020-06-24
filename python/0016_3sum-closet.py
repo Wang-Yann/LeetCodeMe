3f1bc95c3146a82f17fe135ed2e66d2e89256e9a
Rock　　 <rock@get.com.mm>
2020-06-24 10:00:12 +0800

    EveryDay

--- python/0016_3sum-closet.py
+++ python/0016_3sum-closet.py
@@ -8,10 +8,12 @@
 
 from typing import List
 
+import pytest
+
 
 class Solution:
 
-    def threeSumClosest0(self, nums: List[int], target: int) -> int:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
         length = len(nums)
         res = float("inf")
@@ -19,14 +21,17 @@ class Solution:
         for i in range(0, length - 2):
             for j in range(i + 1, length - 1):
                 for k in range(j + 1, length):
-                    vv = nums[i]+nums[j]+ nums[k]
+                    vv = nums[i] + nums[j] + nums[k]
                     if abs(vv - target) < abs(delta):
                         delta = vv - target
                         res = vv
-                        if delta==0:
+                        if delta == 0:
                             return res
 
         return res
+
+
+class Solution1:
 
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
@@ -37,7 +42,7 @@ class Solution:
         while i < length - 2:
             j, k = i + 1, length - 1
             while j < k:
-                v_sum =nums[i] + nums[j] + nums[k]
+                v_sum = nums[i] + nums[j] + nums[k]
                 delta_now = v_sum - target
 
                 if delta_now == 0:
@@ -46,18 +51,20 @@ class Solution:
                     j += 1
                 else:
                     k = k - 1
-
-                if  abs(delta_now) < delta:
+                if abs(delta_now) < delta:
                     delta = abs(delta_now)
                     res = v_sum
             i += 1
         return res
 
 
-if __name__ == '__main__':
-    sol = Solution()
-    sample0 = [-1, 2, 1, -4]
-    sample = [0,1,2]
+@pytest.mark.parametrize("kw,expected", [
+    [dict(nums=[-1, 2, 1, -4], target=1), 2],
+    [dict(nums=[0, 1, 2], target=3), 3],
+])
+def test_solutions(kw, expected):
+    assert Solution().threeSumClosest(**kw) == expected
 
-    print(sol.threeSumClosest(sample0, 1))
-    print(sol.threeSumClosest(sample, 3))
+
+if __name__ == '__main__':
+    pytest.main(["-q", "--color=yes", "--capture=no", __file__])
