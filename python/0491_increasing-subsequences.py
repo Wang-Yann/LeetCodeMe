99d83fd4d57a333f973496af48d55c09d3229da3
Rock　　 <rock@get.com.mm>
2020-08-25 10:00:05 +0800

    Digest

--- python/0491_increasing-subsequences.py
+++ python/0491_increasing-subsequences.py
@@ -26,7 +26,7 @@
 #  Related Topics 深度优先搜索
 
 """
-
+import math
 from typing import List
 
 import pytest
@@ -35,17 +35,17 @@ import pytest
 class Solution1:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         res = set()
-        length = len(nums)
-        if length < 2: return []
+        N = len(nums)
+        if N < 2: return []
 
         def dfs(idx, cur_path):
             if len(cur_path) >= 2:
                 res.add(tuple(cur_path))
-            for i in range(idx + 1, length):
+            for i in range(idx + 1, N):
                 if nums[i] >= nums[idx]:
                     dfs(i, cur_path + [nums[i]])
 
-        for i in range(length - 1):
+        for i in range(N - 1):
             dfs(i, [nums[i]])
         return [list(x) for x in res]
 
@@ -57,17 +57,16 @@ class Solution:
         回溯 　剪枝
         """
         ans = []
-        length = len(nums)
-        if length < 2: return []
+        N = len(nums)
+        if N < 2:
+            return []
 
         def dfs(pos, cur_path):
             if len(cur_path) >= 2:
                 ans.append(list(cur_path))
             lookup = set()
-            for i in range(pos, length):
-                if (not cur_path
-                    or nums[i] >= cur_path[-1]
-                ) and nums[i] not in lookup:
+            for i in range(pos, N):
+                if (not cur_path or nums[i] >= cur_path[-1]) and nums[i] not in lookup:
                     lookup.add(nums[i])
                     cur_path.append(nums[i])
                     dfs(i + 1, cur_path)
@@ -79,15 +78,36 @@ class Solution:
 
 # leetcode submit region end(Prohibit modification and deletion)
 
+class Solution2:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        N = len(nums)
+        if N < 2:
+            return []
+
+        def dfs(pos, last, cur_path):
+            if pos == N:
+                if len(cur_path) >= 2:
+                    ans.append(cur_path[:])
+                return
+            if nums[pos] >= last:
+                cur_path.append(nums[pos])
+                dfs(pos + 1, nums[pos], cur_path)
+                cur_path.pop()
+            if nums[pos] != last:
+                dfs(pos + 1, last, cur_path)
+
+        dfs(0, -math.inf, [])
+        return ans
+
 
 @pytest.mark.parametrize("args,expected", [
     ([4, 6, 7, 7], [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7, 7], [4, 7, 7]])
 ])
 def test_solutions(args, expected):
-    res = sorted(Solution().findSubsequences(args))
-    res1 = sorted(Solution1().findSubsequences(args))
-    assert res == sorted(expected)
-    assert res1 == sorted(expected)
+    assert sorted(Solution().findSubsequences(args)) == sorted(expected)
+    assert sorted(Solution1().findSubsequences(args)) == sorted(expected)
+    assert sorted(Solution2().findSubsequences(args)) == sorted(expected)
 
 
 if __name__ == '__main__':
